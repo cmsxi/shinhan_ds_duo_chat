51140a282fe7aff166775d6cae6db69a74da494e
ohhyeonn <ohhyeon120@gmail.com>
2024-04-11 17:10:40 +0900

    add main

--- src/shinhan_ds_duo_chat/Main.java
+++ src/shinhan_ds_duo_chat/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	static Friend friend = null;
 	static Start start = new Start();
 	static UserInfo userInfo = null;
-
+	static Chat chat = null;
 	public static void main(String[] args) {
 		try {
 			start.logo();
@@ -25,6 +25,7 @@ public class Main {
 			if (session != null) {
 				friend = new Friend(session);
 				userInfo = new UserInfo(session);
+				chat = new Chat(session);
 				break;
 			}
 		}
@@ -33,6 +34,7 @@ public class Main {
 			String menuNo = start.menu();
 			switch (menuNo) {
 			case "1" -> friend.list();
+			case "2" -> chat.list();
 			case "3" -> userInfo.list();
 			}
 
